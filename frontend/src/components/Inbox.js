ec8a7acf828e449ff9a42486fa0e0ca17db4e81f
Omar Jallouq <omarjallouq@gmail.com>
2024-01-16 01:08:55 +0100

    made basic functionality for inbox with email+pass

--- frontend/src/components/Inbox.js
+++ frontend/src/components/Inbox.js
@@ -1,30 +1,71 @@
 import React, {useEffect, useState} from 'react';
+import TemplateBackground from '../styles/template.png';
+import '../styles/inbox.css';
 
 function Inbox() {
-    useEffect( () => {
-        fetchItems();
-    }, []);
-
     const [items, setItems] = useState([]);
-
+    const [filteredItems, setFilteredItems] = useState([]);
+    const [emailFilter, setEmailFilter] = useState('');
+    const [passwordFilter, setPasswordFilter] = useState('');
+  
+    useEffect(() => {
+      fetchItems();
+    }, []);
+  
     const fetchItems = async () => {
-        const data = await fetch('/inbox');
-        const items = await data.json();
-        setItems(items);
+      const data = await fetch('/inbox');
+      const items = await data.json();
+      setItems(items);
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const newFilteredItems = items.filter(
+        (item) =>
+          item.recipient.email === emailFilter && item.recipient.password === passwordFilter
+      );
+      setFilteredItems(newFilteredItems);
     };
 
     return(
         <section>
-            {
-            items.map(item => (
-                <div>
-                    <p>to: {item.recipient.name}</p>
-                    <p>{item.message}</p>
-                    <p>from: {item.sender}</p>
-                    <p>~~~~~~~~</p>
+            <form onSubmit={handleSubmit}>
+                <div id="loginForm">
+                    <div>
+                        <label>Email:</label>
+                        <input type="text" name="emailInput" placeholder="Put your email here" onChange={(e) => setEmailFilter(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <input type="password" name="passwordInput" placeholder="Put your password here" onChange={(e) => setPasswordFilter(e.target.value)}/>
+                    </div>
                 </div>
-            ))
-            }
+                <button type="submit">Submit</button>
+            </form>
+            <div class="cards">
+                {
+                filteredItems.map(item => (
+                    // <div>
+                    //     <p>to: {item.recipient.name}</p>
+                    //     <p>{item.message}</p>
+                    //     <p>from: {item.sender}</p>
+                    //     <p>color: {item.color}</p>
+                    //     <p>~~~~~~~~</p>
+                    // </div>
+                    <div class="card_div" id ="cardDiv" style={{width: '440px', height: '496px', backgroundColor: `${item.color}`, backgroundImage: `url(${TemplateBackground})`}}>
+                        <div class="recipient_div" style={{background: 'transparent'}}>
+                            <span class="to_text" style={{fontSize: '24px', lineHeight: '44px', background: 'transparent'}}>To:</span>
+                            <input class="recipient_input" name="recipientInput" style={{fontSize: '24px', lineHeight: '44px', background:'transparent'}} value={item.recipient.name}/>
+                        </div>
+                        <textarea class="messageTA submit_lightScheme__lLwOA" name="messageInput" placeholder="Type Your Message Here..." style={{fontSize: '44px', lineHeight: '52px'}} value={item.message}></textarea>
+                        <div class="sender_div" style={{background: 'transparent'}}>
+                            <span class="from_text" style={{fontSize: '33px', lineHeight: '44px', background: 'transparent'}}>From: </span>
+                            <input class="sender_name" name="senderInput" placeholder="Enter Name" style={{fontSize: '33px', lineHeight: '44px'}} value={item.sender}/>
+                        </div>
+                    </div>
+                ))
+                }
+            </div>
         </section>
     );
 }

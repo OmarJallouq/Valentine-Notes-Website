9f54a77e4faa62511f752ba4c47937677d784a40
Omar Jallouq <omarjallouq@gmail.com>
2024-01-21 20:06:26 +0100

    Implemented default export in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 // frontend/src/services/api.js
 const apiUrl = process.env.BACKEND_URL;
 
-export const fetchData = async (endpoint) => {
+const fetchData = async (endpoint) => {
   const response = await fetch(`${apiUrl}/${endpoint}`);
   const data = await response.json();
   return data;
 };
+
+export default fetchData;

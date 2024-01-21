4030ff65fc4dd39508fcc48306d5683ca8ee04f6
Omar Jallouq <omarjallouq@gmail.com>
2024-01-21 23:22:20 +0100

    added max length to sender input

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -49,7 +49,7 @@ function Message() {
                         <textarea class="messageTA submit_lightScheme__lLwOA" name="messageInput" maxLength="67" placeholder="Type Your Message Here..." style={{fontSize: '44px', lineHeight: '52px'}} onChange={handleTextAreaChange}></textarea>
                         <div class="sender_div" style={{background: 'transparent'}}>
                             <span class="from_text" style={{fontSize: '33px', lineHeight: '44px', background: 'transparent'}}>From: </span>
-                            <input class="sender_name" name="senderInput" placeholder="Enter Name" style={{fontSize: '33px', lineHeight: '44px', paddingLeft: '6px'}}/>
+                            <input class="sender_name" name="senderInput" maxLength="16" placeholder="Enter Name" style={{fontSize: '33px', lineHeight: '44px', paddingLeft: '6px'}}/>
                         </div>
                     </div>
                     <div class="colors_div">

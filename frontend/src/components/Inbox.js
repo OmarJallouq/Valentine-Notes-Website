407fb84848d49b000c7fc8935c242861d4049060
Omar Jallouq <omarjallouq@gmail.com>
2024-01-29 23:15:19 +0100

    Added Valentines Day Locked message and button

--- frontend/src/components/Inbox.js
+++ frontend/src/components/Inbox.js
@@ -1,19 +1,28 @@
 import React, {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import TemplateBackground from '../styles/template.png';
 import '../styles/inbox.css';
 
 function Inbox() {
+    const navigate = useNavigate();
     const [items, setItems] = useState([]);
     const [users, setUsers] = useState([]);
     const [filteredItems, setFilteredItems] = useState([]);
     const [emailFilter, setEmailFilter] = useState('');
     const [passwordFilter, setPasswordFilter] = useState('');
-    const VALENTINESDAY = true;
+
+    const currentDate = new Date();
+    const VALENTINESDAY = currentDate.getMonth() === 1 && currentDate.getDate() === 14;
   
     useEffect(() => {
       fetchItems();
     }, []);
-  
+    
+    const handleMessageClick = () => {
+        // Navigate to another route within your SPA
+        navigate('/send-a-message');
+    };
+
     const fetchItems = async () => {
       try {
         const response = await fetch('/api/inbox');
@@ -69,36 +78,49 @@ function Inbox() {
 
     return(
         <section>
-            <form class="form" onSubmit={handleSubmit}>
-                <div class="wrapper">
-                    <div class="title">Welcome,<br/><span>Enter credentials to continue</span></div>
-                    <div>
-                        <input class="input" type="text" name="emailInput" placeholder="Put your email here" onChange={(e) => setEmailFilter(e.target.value)}/>
+            {VALENTINESDAY ? (
+                <div>
+                    <form class="form" onSubmit={handleSubmit}>
+                        <div class="wrapper">
+                            <div class="title">Welcome,<br/><span>Enter credentials to continue</span></div>
+                            <div>
+                                <input class="input" type="text" name="emailInput" placeholder="Put your email here" onChange={(e) => setEmailFilter(e.target.value)}/>
+                            </div>
+                            <div>
+                                <input class="input" type="password" name="passwordInput" placeholder="Put your password here" onChange={(e) => setPasswordFilter(e.target.value)}/>
+                            </div>
+                            <button class="button-confirm" type="submit">Submit</button>
+                        </div>
+                    </form>
+                    <div class="cards">
+                        { 
+                        filteredItems.map(item => (
+                            <div class="card_div" id ="cardDiv" style={{width: '440px', height: '496px', backgroundColor: `${item.BGcolor}`, backgroundImage: `url(${TemplateBackground})`, marginBottom: '18px'}}>
+                                <div class="recipient_div" style={{background: 'transparent'}}>
+                                    <span class="to_text" style={{fontSize: '24px', lineHeight: '44px', background: 'transparent'}}>To:</span>
+                                    <input class="recipient_input" name="recipientInput" style={{fontSize: '24px', lineHeight: '44px', background:'transparent', paddingLeft: '6px'}} value={item.recipient.name}/>
+                                </div>
+                                <textarea class="messageTA" name="messageInput" placeholder="Type Your Message Here..." style={{fontSize: '44px', lineHeight: '52px', color: item.TextColor ? item.TextColor : '#fff'}} value={item.message}></textarea>
+                                <div class="sender_div" style={{background: 'transparent'}}>
+                                    <span class="from_text" style={{fontSize: '33px', lineHeight: '44px', background: 'transparent'}}>From: </span>
+                                    <input class="sender_name" name="senderInput" placeholder="Enter Name" style={{fontSize: '33px', lineHeight: '44px', paddingLeft: '6px'}} value={item.sender}/>
+                                </div>
+                            </div>
+                        ))
+                        }
+                        <h3 id="err" hidden='true'>Incorrect Email or Password</h3>
                     </div>
-                    <div>
-                        <input class="input" type="password" name="passwordInput" placeholder="Put your password here" onChange={(e) => setPasswordFilter(e.target.value)}/>
-                    </div>
-                    <button class="button-confirm" type="submit">Submit</button>
                 </div>
-            </form>
-            <div class="cards">
-                { 
-                filteredItems.map(item => (
-                    <div class="card_div" id ="cardDiv" style={{width: '440px', height: '496px', backgroundColor: `${item.BGcolor}`, backgroundImage: `url(${TemplateBackground})`, marginBottom: '18px'}}>
-                        <div class="recipient_div" style={{background: 'transparent'}}>
-                            <span class="to_text" style={{fontSize: '24px', lineHeight: '44px', background: 'transparent'}}>To:</span>
-                            <input class="recipient_input" name="recipientInput" style={{fontSize: '24px', lineHeight: '44px', background:'transparent', paddingLeft: '6px'}} value={item.recipient.name}/>
-                        </div>
-                        <textarea class="messageTA" name="messageInput" placeholder="Type Your Message Here..." style={{fontSize: '44px', lineHeight: '52px', color: item.TextColor ? item.TextColor : '#fff'}} value={item.message}></textarea>
-                        <div class="sender_div" style={{background: 'transparent'}}>
-                            <span class="from_text" style={{fontSize: '33px', lineHeight: '44px', background: 'transparent'}}>From: </span>
-                            <input class="sender_name" name="senderInput" placeholder="Enter Name" style={{fontSize: '33px', lineHeight: '44px', paddingLeft: '6px'}} value={item.sender}/>
-                        </div>
+            ) : (
+                <div style={{paddingTop: '18px'}}>
+                    <div class="wrapper" style={{justifyContent: 'center'}}>
+                        <h3 style={{color: 'white'}}>You can access your messages on the 14th of February! Check back then!</h3>
+                        <button onClick={handleMessageClick} style={{paddingLeft: 24, paddingRight: 24, paddingTop: 10, paddingBottom: 10, background: 'white', borderRadius: 8, border: '3px #FF82A9 solid', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex', margin: 'auto'}}>
+                            <div style={{color: '#FF82A9', backgroundColor: 'transparent', fontSize: 16, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'normal', whiteSpace: 'nowrap', margin: 'auto'}}>Send a Message</div>
+                        </button>
                     </div>
-                ))
-                }
-                <h3 id="err" hidden='true'>Incorrect Email or Password</h3>
-            </div>
+                </div>
+            )}
         </section>
     );
 }

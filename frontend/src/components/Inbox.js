444d9a159e6e0fc90ed11b7b84f55c9ebb1b92f4
Omar Jallouq <omarjallouq@gmail.com>
2024-01-30 16:19:25 +0100

    j

--- frontend/src/components/Inbox.js
+++ frontend/src/components/Inbox.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import TemplateBackground from '../styles/template.png';
+//import TemplateBackground from '../styles/template.png';
 import '../styles/inbox.css';
 
 function Inbox() {

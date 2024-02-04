72ff11d05afd8c050f9d28d117c626bc779a6e36
Omar Jallouq <omarjallouq@gmail.com>
2024-02-04 19:12:46 +0100

    removed whatever i just did jeez calm down

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -179,7 +179,7 @@ function Message() {
                             <input type="hidden" id="textColor" name="textColor" value={textColor} />
                             <div class="recipient_div" style={{background: 'transparent'}}>
                                 <span class="to_text" style={{fontSize: '24px', lineHeight: '44px', background: 'transparent'}}>To:</span>
-                                <select class="recipient_input" name="recipientInput" style={{fontSize: '24px', lineHeight: '44px', background:'transparent', length: '100px'}} onChange={handleRecipientChange}>
+                                <select class="recipient_input" name="recipientInput" style={{fontSize: '24px', lineHeight: '44px', background:'transparent'}} onChange={handleRecipientChange}>
                                     <option value="" disabled={!defaultOptionDisabled} hidden>Select a recipient</option>                                    
                                     {items.map(item => (
                                         <option key={item._id} value={item.name}>

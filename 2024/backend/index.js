a7e8a9d7b05603edfdaa61ad5ab4a37fbf8b1ff1
Omar Jallouq <omarjallouq@gmail.com>
2025-02-02 19:26:21 +0100

    direct url

--- 2024/backend/index.js
+++ 2024/backend/index.js
@@ -7,31 +7,31 @@ require('dotenv/config')
 
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
 
 const allowedOrigins = [process.env.FRONTEND_URL]
 app.use(cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  }));
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
 
 //DB Connection
 mongoose.connect(process.env.DB_URI)
-.then( () => {
+  .then(() => {
     console.log('DB Connected!');
-})
-.catch( (err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const PORT = process.env.PORT || 4000; //Backend Routing Port
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- 2024/frontend/src/components/Inbox.js
+++ 2024/frontend/src/components/Inbox.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TemplateBackground from '../styles/template.png';
 import '../styles/inbox.css';
@@ -13,32 +13,32 @@ function Inbox() {
     const VALENTINESDAY = true;
 
     useEffect(() => {
-      fetchItems();
+        fetchItems();
     }, []);
-    
+
     const handleMessageClick = () => {
         // Navigate to another route within your SPA
         navigate('/send-a-message');
     };
 
     const fetchItems = async () => {
-      try {
-        const response = await fetch('/api/FKJDKLFJLAKDJFLKAJDKFLAJDKFA');
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+        try {
+            const response = await fetch('https://bai-valentines.onrender.com/FKJDKLFJLAKDJFLKAJDKFLAJDKFA');
+            const data = await response.json();
+            setItems(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
 
-      try {
-        const response = await fetch('/api/kjlfdsakljLFKJADKLFJAKLJDAk2938192');
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+        try {
+            const response = await fetch('https://bai-valentines.onrender.com/kjlfdsakljLFKJADKLFJAKLJDAk2938192');
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     };
-  
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -47,26 +47,26 @@ function Inbox() {
                 user.email === emailFilter && user.password === passwordFilter
         );
 
-        if ((emailFilter === "admin" && passwordFilter === "Admin_123") || userSelected){
+        if ((emailFilter === "admin" && passwordFilter === "Admin_123") || userSelected) {
             document.getElementById("err").hidden = true;
             const newFilteredItems = items.filter(
                 (item) =>
                     (item.recipient?.email === emailFilter && item.recipient?.password === passwordFilter) || item.sender === "Omar & Velina" //TODO ADD TEAM NAME OR WHATEVER
             );
-            if(VALENTINESDAY){
+            if (VALENTINESDAY) {
                 setFilteredItems(newFilteredItems);
             }
-            if (emailFilter === "admin" && passwordFilter === "Admin_123"){
+            if (emailFilter === "admin" && passwordFilter === "Admin_123") {
                 setFilteredItems(items);
             }
         }
         else {
             document.getElementById("err").hidden = false;
             setFilteredItems([]);
-            if (!emailFilter){
+            if (!emailFilter) {
                 document.getElementById("err").innerHTML = "Please enter an email";
             }
-            else if(!passwordFilter){
+            else if (!passwordFilter) {
                 document.getElementById("err").innerHTML = "Please enter a passwod";
             }
             else {
@@ -74,18 +74,18 @@ function Inbox() {
             }
         }
     };
-    return(
+    return (
         <section>
             {VALENTINESDAY ? (
                 <div>
                     <form class="form" onSubmit={handleSubmit}>
                         <div class="wrapper">
-                            <div class="title">Welcome,<br/><span>Enter credentials to continue (Text Omar/Velina if you don't have yours!)</span></div>
+                            <div class="title">Welcome,<br /><span>Enter credentials to continue (Text Omar/Velina if you don't have yours!)</span></div>
                             <div>
-                                <input class="input" type="text" name="emailInput" placeholder="Put your email here" onChange={(e) => setEmailFilter(e.target.value)}/>
+                                <input class="input" type="text" name="emailInput" placeholder="Put your email here" onChange={(e) => setEmailFilter(e.target.value)} />
                             </div>
                             <div>
-                                <input class="input" type="password" name="passwordInput" placeholder="Put your password here" onChange={(e) => setPasswordFilter(e.target.value)}/>
+                                <input class="input" type="password" name="passwordInput" placeholder="Put your password here" onChange={(e) => setPasswordFilter(e.target.value)} />
                             </div>
                             <button class="button-confirm" type="submit">Submit</button>
                         </div>
@@ -93,17 +93,17 @@ function Inbox() {
                     <div class="cards">
                         {
                             filteredItems.map(item => (
-                                <div key={item.id} class="card_div" id ="cardDiv" style={{width: '440px', height: '496px', backgroundColor: `${item.BGcolor}`, backgroundImage: `url(${TemplateBackground})`, marginBottom: '18px'}}>
-                                {item.recipient && (
-                                    <div class="recipient_div" style={{background: 'transparent'}}>
-                                        <span class="to_text" style={{fontSize: '24px', lineHeight: '44px', background: 'transparent'}}>To:</span>
-                                        <input class="recipient_input" name="recipientInput" style={{fontSize: '24px', lineHeight: '44px', background:'transparent', paddingLeft: '6px'}} value={item.recipient.name}/>
-                                    </div>
-                                )}
-                                    <textarea class="messageTA" name="messageInput" placeholder="Type Your Message Here..." style={{fontSize: '44px', lineHeight: '52px', color: item.TextColor ? item.TextColor : '#fff'}} value={item.message}></textarea>
-                                    <div class="sender_div" style={{background: 'transparent'}}>
-                                        <span class="from_text" style={{fontSize: '33px', lineHeight: '44px', background: 'transparent'}}>From: </span>
-                                        <input class="sender_name" name="senderInput" placeholder="Enter Name" style={{fontSize: '33px', lineHeight: '44px', paddingLeft: '6px'}} value={item.sender}/>
+                                <div key={item.id} class="card_div" id="cardDiv" style={{ width: '440px', height: '496px', backgroundColor: `${item.BGcolor}`, backgroundImage: `url(${TemplateBackground})`, marginBottom: '18px' }}>
+                                    {item.recipient && (
+                                        <div class="recipient_div" style={{ background: 'transparent' }}>
+                                            <span class="to_text" style={{ fontSize: '24px', lineHeight: '44px', background: 'transparent' }}>To:</span>
+                                            <input class="recipient_input" name="recipientInput" style={{ fontSize: '24px', lineHeight: '44px', background: 'transparent', paddingLeft: '6px' }} value={item.recipient.name} />
+                                        </div>
+                                    )}
+                                    <textarea class="messageTA" name="messageInput" placeholder="Type Your Message Here..." style={{ fontSize: '44px', lineHeight: '52px', color: item.TextColor ? item.TextColor : '#fff' }} value={item.message}></textarea>
+                                    <div class="sender_div" style={{ background: 'transparent' }}>
+                                        <span class="from_text" style={{ fontSize: '33px', lineHeight: '44px', background: 'transparent' }}>From: </span>
+                                        <input class="sender_name" name="senderInput" placeholder="Enter Name" style={{ fontSize: '33px', lineHeight: '44px', paddingLeft: '6px' }} value={item.sender} />
                                     </div>
                                 </div>
                             ))
@@ -112,11 +112,11 @@ function Inbox() {
                     <h3 id="err" hidden='true'>Incorrect Email or Password</h3>
                 </div>
             ) : (
-                <div style={{paddingTop: '18px'}}>
-                    <div class="wrapper" style={{justifyContent: 'center'}}>
-                        <h3 style={{color: 'white'}}>You can access your messages on the 14th of February! Check back soon!</h3>
-                        <button onClick={handleMessageClick} style={{paddingLeft: 24, paddingRight: 24, paddingTop: 10, paddingBottom: 10, background: 'white', borderRadius: 8, border: '3px #FF82A9 solid', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex', margin: 'auto'}}>
-                            <div style={{color: '#FF82A9', backgroundColor: 'transparent', fontSize: 16, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'normal', whiteSpace: 'nowrap', margin: 'auto'}}>Send a Message</div>
+                <div style={{ paddingTop: '18px' }}>
+                    <div class="wrapper" style={{ justifyContent: 'center' }}>
+                        <h3 style={{ color: 'white' }}>You can access your messages on the 14th of February! Check back soon!</h3>
+                        <button onClick={handleMessageClick} style={{ paddingLeft: 24, paddingRight: 24, paddingTop: 10, paddingBottom: 10, background: 'white', borderRadius: 8, border: '3px #FF82A9 solid', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex', margin: 'auto' }}>
+                            <div style={{ color: '#FF82A9', backgroundColor: 'transparent', fontSize: 16, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'normal', whiteSpace: 'nowrap', margin: 'auto' }}>Send a Message</div>
                         </button>
                     </div>
                 </div>
